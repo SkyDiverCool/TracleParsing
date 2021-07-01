6d22c0ecf852ea67d95a845a1981b6b95b461d4b
Eyjafjallajokull Wissotsky <SkyDiverCool@users.noreply.github.com>
2021-07-01 13:38:48 +0300

    -^+r

--- tracle.py
+++ tracle.py
@@ -1,11 +1,12 @@
 import allmyrequests
 import parsergarden
-
-raw_searchterm = "ucoSWBde5iI"
+# usage example where we get the videos uploaded by userid ucoSWBde5iI(Wissotsky) and print the dataframe
+raw_searchterm = "ucoSWBde5iI" 
 # search,video,user
 page = allmyrequests.amr_request(raw_searchterm, "user")
 
-# search,video,video_comments,user
+# search,video,user,user_videos
+# user_videos is parsed from user page input
 videodf = parsergarden.pg_parse(page, "user_videos")
 
 print(videodf)
